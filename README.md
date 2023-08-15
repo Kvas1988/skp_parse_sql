79bba1e7fb78e28c1581b1dd6faa483bb0e1d174
Шохин Александр Николаевич <Aleksandr.Shohin@ms.snq.ru>
2023-08-15 19:24:34 +0300

    feat: checking fields and tables with oracle db

--- README.md
+++ README.md
@@ -5,19 +5,24 @@
 git clone https://github.com/Kvas1988/skp_parse_sql.git
 pip install -r requirements.txt
 ```
+Also you should create config.toml file with DB_CONN parameter:
+```toml
+DB_CONN = 'oracle://user:password@server.addr.com/dbname'
+```
 
 ## Usage
 You can run script with given path to sql-file
 ```bash
 python parse_reports.py -f path/to/query.sql 
+python parse_reports.py -f path/with/quries/
 ```
 
 Or via python api
 ```python
+from sqlalchemy.engine import create_engine
 from parse_reports import Parser
 
-p = Parser("path/to/query")
-p.parse()
-print(p.fields)
-print(p.tables)
+engine = create_engine("YOUR_DB_CREDS")
+p = Parser("path/to/query", engine)
+df = p.parse() # return is pandas DataFrame
 ```
--- parse_reports.py
+++ parse_reports.py
@@ -1,23 +1,34 @@
 import re
 import sqlparse
 import argparse
+from sqlalchemy import create_engine, text, bindparam
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine import Engine
+import pandas as pd
+from os import path, listdir
+from datetime import datetime
+import tomli
+
 
 class Parser():
-    def __init__(self, path: str) -> None:
+    def __init__(self, path: str, engine: Engine) -> None:
+        self.path = path
+        self.engine = engine
+        self.data = []
         self.tables = []
         self.fields = set()
         self.state = "IDLE"
         self.query = self.read_file(path)
-        
+
         self.tokens = sqlparse.parse(self.query)
         if len(self.tokens) > 1:
             print("got", len(self.tokens), "queries")
         self.tokens = self.tokens[0]
 
-        self.syntax_words = ['select', 'from', 'case', 'trunc', 'as', 'like', \
-                             'trunc', 'max', 'min', 'nvl', 'upper', 'avg',    \
-                             'sum', 'when', 'else', 'then', 'end', 'and',     \
-                             '*', 'is', 'null',  '', 'not', 'nvl2', ',', \
+        self.syntax_words = ['select', 'from', 'case', 'trunc', 'as', 'like',
+                             'trunc', 'max', 'min', 'nvl', 'upper', 'avg',
+                             'sum', 'when', 'else', 'then', 'end', 'and',
+                             '*', 'is', 'null',  '', 'not', 'nvl2', ',',
                              '=', '+', '-', '>', '<', '>=', '<=', '!=', "/"]
 
     def read_file(self, path: str) -> str:
@@ -39,14 +50,19 @@ class Parser():
     def handle_field(self, token):
         arr = token.value.lower().split()
         arr = [w for t in arr for w in re.split('\(|\)', t)]
-        arr = [w for w in arr if not w in self.syntax_words]
+        arr = [w for w in arr if w not in self.syntax_words]
         arr = [w for w in arr if not re.match("[0-9]|'\d{2}\.\d{2}\.\d{4}'|'.*'", w)]
         arr = [w for w in arr if not re.match('".*"|\'.*\'', w)]
         arr = [re.sub('-|.*\.|,', '', w) for w in arr]
         for w in set(arr):
             self.fields.add(w)
 
-    def parse(self, tokens = None):
+    def parse(self):
+        self.__parse_query()
+        self.check_dwh()
+        return self.to_df()
+
+    def __parse_query(self, tokens=None):
         if not tokens:
             tokens = self.tokens
 
@@ -65,7 +81,7 @@ class Parser():
 
             elif isinstance(token, sqlparse.sql.IdentifierList) or \
                  isinstance(token, sqlparse.sql.Parenthesis):
-                self.parse(tokens = token)
+                self.__parse_query(tokens=token)
 
             elif isinstance(token, sqlparse.sql.Identifier):
                 # print(i, token.value)
@@ -75,23 +91,82 @@ class Parser():
         print(self.tables)
         print(self.fields)
 
+    def check_dwh(self):
+        params = dict()
+        params['columns'] = tuple(self.fields)
+        params['columns'] = [c.upper() for c in params['columns']]
+
+        tables = tuple([re.split('\.', t, maxsplit=1) for t in self.tables])
+        params['owners'] = tuple(set([t[0].upper() for t in tables]))
+
+        params['tables'] = tuple([t[1] if len(t) > 1 else t[0] for t in tables])
+        params['tables'] = [t.upper() for t in params['tables']]
+
+        q = """
+                 SELECT t.OWNER,
+                        t.TABLE_NAME,
+                        t.owner || '.' || t.table_name fullname,
+                        t.COLUMN_NAME
+                FROM all_tab_columns t
+                 WHERE t.OWNER in :owners
+                 AND t.TABLE_NAME in :tables
+                 AND t.COLUMN_NAME in :columns
+                 """
+
+        t = text(q)
+        t = t.bindparams(bindparam('owners', expanding=True))
+        t = t.bindparams(bindparam('tables', expanding=True))
+        t = t.bindparams(bindparam('columns', expanding=True))
+
+        session = sessionmaker(self.engine)
+        with session.begin() as s:
+            data = s.execute(t, params).all()
+
+        for row in data:
+            self.data.append([v for v in row])
+
+    def to_df(self):
+        headers = ['owner', 'table_name', 'table_fullname', 'column']
+        df = pd.DataFrame(self.data, columns=headers)
+        df['sql_path'] = self.path
+        return df
+
+
+def handle_folder(folder: str, engine: Engine):
+    df = pd.DataFrame()
+    for f in listdir(folder):
+        print(f)
+        fp = path.join(folder, f)
+        sql_parser = Parser(fp, engine)
+        df = pd.concat([df, sql_parser.parse()])
+    ts = datetime.now().strftime("%Y%m%d_%H%M%S")
+    df.to_excel('result_' + ts + '.xlsx', index=False)
+
 
 def main():
     parser = argparse.ArgumentParser() 
     parser.add_argument("-f", "--filename", required=False)
     args = parser.parse_args()
 
+    try:
+        with open("config.toml", 'rb') as f:
+            conf = tomli.load(f)
+        engine = create_engine(conf['DB_CONN'])
+    except (OSError, KeyError):
+        print("ERROR: CREATE config.toml WITH DWH_CONN PARAMETER")
+        quit()
+
     if args.filename:
-        print(args.filename)
-        sql_parser = Parser(args.filename)
-        sql_parser.parse()
-        sql_parser.print_output()
+        if path.isfile(args.filename):
+            sql_parser = Parser(args.filename, engine)
+            df = sql_parser.parse()
+            ts = datetime.now().strftime("%Y%m%d_%H%M%S")
+            df.to_excel('result_' + ts + '.xlsx', index=False)
+        else:
+            handle_folder(args.filename, engine)
     else:
-        # sql_parser = Parser("test.sql")
-        sql_parser = Parser("margin_01.sql")
-        sql_parser.parse()
-        sql_parser.print_output()
-    
+        print("RUN SCRIPT WITH -f filepath.sql (OR -f folder\)")
+
 
 if __name__ == "__main__":
     main()
